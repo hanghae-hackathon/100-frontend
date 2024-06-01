cc3af092ef4ed89291ac434553b08e52ac3a892d
oauch <lgm9515@naver.com>
2024-06-01 13:09:07 +0900

    feat: DogImages 컴포넌트 수정

--- src/components/dogs/DogsImages.tsx
+++ src/components/dogs/DogsImages.tsx
@@ -1,18 +1,28 @@
+import { useNavigate } from "@tanstack/react-router";
 import { FunctionComponent } from "../../common/types";
 
 type DogsImagesProps = {
-	src: string[];
+	src: { id: number; img: string }[];
 };
 
 export const DogsImages = ({ src }: DogsImagesProps): FunctionComponent => {
+	const navigate = useNavigate();
+
 	return (
 		<>
 			{src.map((val) => (
-				<img
-					className="w-[150px] h-[150px] rounded-[30px] mt-[20px]"
-					src={val}
-					alt={"결과 이미지"}
-				/>
+				<button
+					key={val.id}
+					onClick={() =>
+						navigate({ to: `/dogs/$id/detail`, params: { id: String(val.id) } })
+					}
+				>
+					<img
+						className="w-[150px] h-[150px] rounded-[30px] mt-[20px]"
+						src={val.img}
+						alt={"결과 이미지"}
+					/>
+				</button>
 			))}
 		</>
 	);
